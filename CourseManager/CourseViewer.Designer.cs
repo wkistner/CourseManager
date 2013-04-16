8b645010b56cdbd463e0176f95048f493ea2b8ef
Wayne K <waynek007@outlook.com>
2013-04-15 21:12:06 -0400

    Test commit

---
+++ CourseManager/CourseViewer.Designer.cs
@@ -0,0 +1,88 @@
+﻿namespace CourseManager
+{
+    partial class CourseViewer
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.departmentList = new System.Windows.Forms.ComboBox();
+            this.closeForm = new System.Windows.Forms.Button();
+            this.courseGridView = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.courseGridView)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // departmentList
+            // 
+            this.departmentList.FormattingEnabled = true;
+            this.departmentList.Location = new System.Drawing.Point(27, 61);
+            this.departmentList.Name = "departmentList";
+            this.departmentList.Size = new System.Drawing.Size(217, 24);
+            this.departmentList.TabIndex = 0;
+            this.departmentList.SelectedIndexChanged += new System.EventHandler(this.departmentList_SelectedIndexChanged);
+            // 
+            // closeForm
+            // 
+            this.closeForm.Location = new System.Drawing.Point(524, 377);
+            this.closeForm.Name = "closeForm";
+            this.closeForm.Size = new System.Drawing.Size(75, 23);
+            this.closeForm.TabIndex = 1;
+            this.closeForm.Text = "Close";
+            this.closeForm.UseVisualStyleBackColor = true;
+            this.closeForm.Click += new System.EventHandler(this.closeForm_Click);
+            // 
+            // courseGridView
+            // 
+            this.courseGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.courseGridView.Location = new System.Drawing.Point(264, 61);
+            this.courseGridView.Name = "courseGridView";
+            this.courseGridView.RowTemplate.Height = 24;
+            this.courseGridView.Size = new System.Drawing.Size(351, 293);
+            this.courseGridView.TabIndex = 2;
+            // 
+            // CourseViewer
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(664, 426);
+            this.Controls.Add(this.courseGridView);
+            this.Controls.Add(this.closeForm);
+            this.Controls.Add(this.departmentList);
+            this.Name = "CourseViewer";
+            this.Text = "Course Viewer";
+            this.Load += new System.EventHandler(this.CourseViewer_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.courseGridView)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox departmentList;
+        private System.Windows.Forms.Button closeForm;
+        private System.Windows.Forms.DataGridView courseGridView;
+    }
+}
+
